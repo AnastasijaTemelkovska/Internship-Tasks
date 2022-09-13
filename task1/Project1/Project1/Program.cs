b06d5a0e57d14a5653d2c09b7b47f7e96a82a571
AnastasijaTemelkovska <80644506+AnastasijaTemelkovska@users.noreply.github.com>
2022-09-13 12:29:50 +0200

    Add files via upload

---
+++ task1/Project1/Project1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Task1
+{
+    
+       
+    class Program
+    {
+        static void NumberStats(string number)
+        {
+            
+            double m = double.Parse(number);
+            int n = (int)m;
+
+            Console.WriteLine("Stats for number: " + number);
+            //Find out if the number is negative or positive
+            if (m == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if (m > 0)
+                {
+                    Console.WriteLine("Positive");
+                }
+                else
+                {
+                    Console.WriteLine("Negative");
+                }
+            }
+            //Find out if the number is decimal or integer
+            if (n == m)
+            {
+                Console.WriteLine("Integer");
+            }
+            else
+            {
+                Console.WriteLine("Decimal");
+            }
+            //Find out if the number is odd or even
+            if ((m % 2) == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+
+            static void Main(string[] args)
+        {
+         
+        //  Console.WriteLine("Input number");
+            String number = Console.ReadLine();
+
+
+            NumberStats(number);
+          
+            }
+
+           
+        
+    }
+}
---
+++ task2/Task2.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Task2
+{
+    public class User
+    {
+        int Id;
+        String username;
+        String password;
+        string[] messages = {};
+
+        public User(int Id, string username, string password, string[] messages)
+        {
+            this.Id = Id;
+            this.username = username;
+            this.password = password;
+            this.messages = messages;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+        public String getUserName()
+        {
+            return username;
+        }
+        public String getPassword()
+        {
+            return password;
+        }
+
+        public String[] getMessages()
+        {
+            return messages;
+        }
+        public void setId(int newId)
+        {
+            Id = newId;
+        }
+        public void setUsername(String newUsername)
+        {
+            username = newUsername;
+        }
+        public void setPassword(String newPassword)
+        {
+            password = newPassword;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] messages = { "Hello!", "How are you?"};
+            List<User> Users = new List<User>();
+            Users.Add(new User(23, "user1", "password1", messages));
+            Users.Add(new User(44, "bokiboki ", "pass2", messages));
+            Users.Add(new User(1, "markom", "pass3", messages));
+
+            //Register a new user
+            void Register()
+            {
+                Console.WriteLine("Enter your ID: ");
+                int ID = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter your username: ");
+                string username1 = Console.ReadLine();
+                Console.WriteLine("Enter your password: ");
+                string password1 = Console.ReadLine();
+
+                bool existing = false;
+                foreach (User u in Users)
+                {
+                    string user1 = u.getUserName();
+                    if (user1.Equals(username1))
+                    {
+                        existing = true;
+                        Console.WriteLine("You are alredy registred");
+
+                    }
+                }
+                if (existing == false)
+                {
+
+                    Users.Add(new User(ID, username1, password1, messages));
+                    Console.WriteLine("Your registration was successful!");
+                    foreach (User us in Users)
+                    {
+                        int id = us.getId();
+                        string user2 = us.getUserName();
+                        Console.WriteLine(id + " " + " " + user2);
+
+                    }
+
+
+                }
+                LogIn();
+
+            }
+
+            // Login user 
+            void LogIn()
+            {
+                Console.WriteLine("Please enter your username: ");
+                string username = Console.ReadLine();
+                Console.WriteLine("Please enter your password: ");
+                string password = Console.ReadLine();
+
+                bool existing = false;
+
+                foreach(User u in Users)
+                {
+                    string user = u.getUserName();
+                    string pass = u.getPassword();
+                    if (user.Equals(username) && pass.Equals(password))
+                    {
+                        existing = true;
+                        Console.WriteLine("Welcome " + user + ". Here are your messages: ");
+                        string[] mess = u.getMessages();
+                        foreach(string m in mess)
+                        {
+                            Console.WriteLine(m);
+                        }
+                    }
+                }
+                if (existing == false)
+                {
+                    Console.WriteLine("Wrong username or password. Please try again!");
+                    WelcomeApp();
+                }
+            }
+
+            //Main app
+            void WelcomeApp()
+            {
+                Console.WriteLine("Welcome!");
+                Console.WriteLine("Choose one of the following options:");
+                Console.WriteLine("1. Log in");
+                Console.WriteLine("2. Register");
+                int option1 = 0;
+                try
+                {
+                    option1 = int.Parse(Console.ReadLine());
+                }
+                catch { }
+                if (option1 == 1) { LogIn(); }
+                else if (option1 == 2) { Register(); }
+                else { option1 = 0; }
+            }
+
+            WelcomeApp();
+
+        }
+    }
+}
+
